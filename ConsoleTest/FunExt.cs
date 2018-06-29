952331b50f39837f1489e8fde28bc06d372c22fb
Stanislav Gulis <stanislav.gulis@gmail.com>
2018-06-29 22:33:34 +0200

    Some type finally works with conditional expressions

--- ConsoleTest/FunExt.cs
+++ ConsoleTest/FunExt.cs
@@ -6,17 +6,17 @@ namespace ConsoleTest
     public static class FunExt
     {
 
-        // public static void Compare()
-        // {
-        //     var x = Some("x");
-        //     var n = None;
+        public static void Compare()
+        {
+            var x = Some("x");
+            var n = None;
 
-        //     var b = (x == n);
-        // }
+            var b = (x == n);
+        }
 
-        // public static Maybe<string> ReadFile(string path) =>
-        //     !string.IsNullOrEmpty(path) ? Some("text") :
-        //     None;
+        public static Option<string> ReadFile(string path) =>
+            !string.IsNullOrEmpty(path) ? Some("text") :
+            None;
 
     }
 }
